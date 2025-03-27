238f0b8cb41a7e044387fc153a84adfb37568365
Tcaciuc Raluca Amalia <tcaciucraluca2000@gmail.com>
2025-03-27 08:45:50 +0200

    backend complet

---
+++ InterfataUtilizator_WindowsForms/Form1.Designer.cs
@@ -0,0 +1,64 @@
+﻿namespace InterfataUtilizator_WindowsForms
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        private void InitializeComponent()
+        {
+            this.lblTitlu = new System.Windows.Forms.Label();
+            this.lblNume = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+
+            // 
+            // lblTitlu
+            // 
+            this.lblTitlu.AutoSize = true;
+            this.lblTitlu.Location = new System.Drawing.Point(50, 20);
+            this.lblTitlu.Name = "lblTitlu";
+            this.lblTitlu.Size = new System.Drawing.Size(35, 13);
+            this.lblTitlu.Text = "Titlu";
+
+            // 
+            // lblNume
+            // 
+            this.lblNume.AutoSize = true;
+            this.lblNume.Location = new System.Drawing.Point(50, 60);
+            this.lblNume.Name = "lblNume";
+            this.lblNume.Size = new System.Drawing.Size(40, 13);
+            this.lblNume.Text = "Nume";
+
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(400, 300);
+            this.Controls.Add(this.lblTitlu);
+            this.Controls.Add(this.lblNume);
+            this.Text = "Interfață Utilizator";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+    }
+}
+
---
+++ InterfataUtilizator_WindowsForms/Form1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using LibrarieModele;
+using NivelStocareDate;
+
+namespace InterfataUtilizator_WindowsForms
+{
+    public partial class Form1: Form
+    {
+        private Label lblTitlu;
+        private Label lblNume;
+
+        public Form1()
+        {
+
+            this.Text = "Interfață Utilizator";
+            this.Width = 400;
+            this.Height = 300;
+
+            lblTitlu = new Label();
+            lblTitlu.Text = "Titlu";
+            lblTitlu.Top = 20;
+            lblTitlu.Left = 50;
+            lblTitlu.AutoSize = true;
+            lblTitlu.Font = new System.Drawing.Font("Arial", 14, System.Drawing.FontStyle.Bold);
+
+            lblNume = new Label();
+            lblNume.Text = "Nume";
+            lblNume.Top = 60;
+            lblNume.Left = 50;
+            lblNume.AutoSize = true;
+            lblNume.Font = new System.Drawing.Font("Arial", 12);
+
+            this.Controls.Add(lblTitlu);
+            this.Controls.Add(lblNume);
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
